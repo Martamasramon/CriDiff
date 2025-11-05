4dadb0d024c0f3745f15c791d2244b3ccc78f34a
martamasramon <marta.masramon@gmail.com>
2025-11-05 13:49:39 +0000

    full env paths

--- pretrain_controlnet.sh
+++ pretrain_controlnet.sh
@@ -7,7 +7,7 @@
 #$ -V
 
 #$ -wd /cluster/project7/ProsRegNet_CellCount/CriDiff/
-#$ -N Pretrain_basic
+#$ -N Train_controlnet
 
 date
 nvidia-smi
@@ -15,11 +15,11 @@ nvidia-smi
 export PATH=/share/apps/python-3.9.5-shared/bin:$PATH
 export LD_LIBRARY_PATH=/share/apps/python-3.9.5-shared/lib:$LD_LIBRARY_PATH
 
-source CriDiff_env/bin/activate
-export PATH="CriDiff_env/bin:$PATH"
+source /cluster/project7/ProsRegNet_CellCount/CriDiff/CriDiff_env/bin/activate
+export PATH="/cluster/project7/ProsRegNet_CellCount/CriDiff/CriDiff_env/bin:$PATH"
 
 cd diffusion_basic
 
-python3 train.py --results_folder './test_x4' --use_T2W --upsample --down 4 
+python3 train.py --results_folder './test_controlnet' --controlnet --down 8 --checkpoint ./pretrain_down8/model-best.pt
 
 date
